66ee3555c097724ac797d9a4930073e1cdf927e5
Jonas Norling <jonas@elemental.se>
2026-02-09 16:18:40 +0100

    Fix some codec config
    Have I ever shifted a number in the right direction on the first try?
    
    Now we get both left and right channels. There are still issues with
    the volume being 10000% sometimes.

--- src/audio.c
+++ src/audio.c
@@ -1,4 +1,5 @@
 #include "audio.h"
+#include <math.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2s.h>
@@ -8,9 +9,10 @@ LOG_MODULE_REGISTER(audio);
 
 
 #define BYTES_PER_SAMPLE 2
-#define SAMPLES_PER_BLOCK 100
+#define SAMPLES_PER_BLOCK 1024
 #define BLOCK_SIZE  (BYTES_PER_SAMPLE * SAMPLES_PER_BLOCK)
 #define BLOCK_COUNT 16
+#define SAMPLE_RATE 48000
 K_MEM_SLAB_DEFINE_IN_SECT_STATIC(mem_slab, __nocache, BLOCK_SIZE, BLOCK_COUNT, 4);
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
@@ -40,29 +42,37 @@ static int config_codec(void)
 	}
 	LOG_INF("SGTL5000 chip id = %x", (unsigned)value);
 
-	// Values to program in registers. Mostly based on control_sgtl5000 from Teensy Audio lib
+	/*
+	 * Values to program in registers.
+	 * Mostly based on control_sgtl5000 from Teensy Audio lib.
+	 * The Teensy audio adapter board is straight forward:
+	 *   VDDD 1.8V supplied by an LDO
+	 *   VDDIO 3.3V from Teensy
+	 *   VDDA 3.3V from Teensy
+	 */
 	static const uint16_t incantations[][2] = {
-		{ 0x0030, 0x4060 }, // CHIP_ANA_POWER: external 1.8V VDDD
-		{ 0x0026, 0x006c }, // CHIP_LINREG_CTRL: VDDA and VDDIO high
+		{ 0x0030, 0x4260 }, // CHIP_ANA_POWER: external 1.8V VDDD
+		{ 0x0030, 0x4a60 }, // CHIP_ANA_POWER: enable charge pump
+		{ 0x0026, 0x006c }, // CHIP_LINREG_CTRL: VDDA and VDDIO >3.1V
 		{ 0x0028, 0x01f2 }, // CHIP_REF_CTRL: Bandgap reference settings
 		{ 0x002c, 0x0f22 }, // CHIP_LINE_OUT_CTRL
 		{ 0x003c, 0x4446 }, // CHIP_SHORT_CTRL: headphone short detector
-		{ 0x0024, 0x0137 }, // CHIP_ANA_CTRL: mute line out, headphones, adc. Line in on ADC.
-		{ 0x0030, 0x40ff }, // CHIP_ANA_POWER: power up it adc and dac and hp
+		{ 0x0024, 0x0133 }, // CHIP_ANA_CTRL
+		{ 0x0030, 0x6aff }, // CHIP_ANA_POWER: power it up
 		{ 0x0002, 0x0073 }, // CHIP_DIG_POWER: power it up
 		{ 0x002e, 0x1d1d }, // CHIP_LINE_OUT_VOL 1.3 Vpp
-		{ 0x0004, 0x0004 }, // CHIP_CLK_CTRL: 44.1 kHz 256*Fs
+		{ 0x0004, 0x0008 }, // CHIP_CLK_CTRL: 48 kHz 256*Fs
 		{ 0x0006, 0x0030 }, // CHIP_I2S_CTRL: 16 bit
 		{ 0x0022, 0x1818 }, // CHIP_ANA_HP_CTRL: volume low
-		{ 0x0024, 0x0026 }, // CHIP_ANA_CTRL: unmute headphones
+		{ 0x0024, 0x0026 }, // CHIP_ANA_CTRL: unmute headphones, line, adc
 		{ 0x000e, 0x0000 }, // CHIP_ADCDAC_CTRL: unmute DACs
 	};
 
 	for (int i = 0; i < ARRAY_SIZE(incantations); i++) {
 		uint8_t data[4] = {
-			incantations[i][0] << 8,
+			incantations[i][0] >> 8,
 			incantations[i][0] & 0xff,
-			incantations[i][1] << 8,
+			incantations[i][1] >> 8,
 			incantations[i][1] & 0xff,
 		};
 		ret = i2c_write(i2c_dev, data, sizeof(data), I2C_ADDR);
@@ -102,7 +112,7 @@ int audio_init(void)
 		.channels = 2,
 		.format = I2S_FMT_DATA_FORMAT_I2S,
 		.options = I2S_OPT_BIT_CLK_MASTER | I2S_OPT_FRAME_CLK_MASTER,
-		.frame_clk_freq = 44100,
+		.frame_clk_freq = SAMPLE_RATE,
 		.mem_slab = &mem_slab,
 		.block_size = BLOCK_SIZE,
 		.timeout = 2000,
@@ -119,7 +129,7 @@ int audio_init(void)
 	}
 
 	LOG_INF("Starting I2S streaming");
-	static uint16_t data[SAMPLES_PER_BLOCK];
+	static int16_t data[SAMPLES_PER_BLOCK];
 	ret = i2s_buf_write(i2s_dev_tx, data, BLOCK_SIZE);
 	if (ret < 0) {
 		LOG_ERR("Failed to write data: %d\n", ret);
@@ -137,9 +147,13 @@ int audio_init(void)
 		return 1;
 	}
 
+	unsigned sample_counter = 0;
 	while (1) {
-		for (int i = 0; i < SAMPLES_PER_BLOCK; i++) {
-			data[i] = i;
+		memset(data, 0, sizeof(data));
+		for (int i = 0; i < SAMPLES_PER_BLOCK; i += 2) {
+			data[i] = 1000 * sin(220 * 6.28 * sample_counter / SAMPLE_RATE);
+			data[i+1] = 1000 * sin(230 * 6.28 * sample_counter / SAMPLE_RATE);
+			sample_counter++;
 		}
 		ret = i2s_buf_write(i2s_dev_tx, data, BLOCK_SIZE);
 		if (ret < 0) {
